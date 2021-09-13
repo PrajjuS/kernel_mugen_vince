f52b5164de92e4317840d0177a55751470a9dbab
gamehackerever <gamehackerever@gmail.com>
2021-09-13 20:17:04 +0530

    sched: Fine Tune Ratelimits
    Signed-off-by: gamehackerever <gamehackerever@gmail.com>
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -878,8 +878,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 	 * intializing up_rate/down_rate to 0 explicitly in kernel
 	 * since WALT expects so by default.
 	 */
-	tunables->up_rate_limit_us = 0;
-	tunables->down_rate_limit_us = 0;
+	tunables->up_rate_limit_us = 50000;
+	tunables->down_rate_limit_us = 100;
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
 	tunables->hispeed_freq = 0;
 
